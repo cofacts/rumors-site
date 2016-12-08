48f0b7d1b9c4c1ecd7cf58eb3f6230c89d8493bc
MrOrz <johnsonliang7@gmail.com>
2016-12-08 21:59:10 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default () => {
+  return <div>Hello world!</div>
+}
+
---
+++ pages/pu/ooo.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default () => <div>puu/livereload~一樣沒有 module name plugin 但看起來整個 DOM 都是 next 處理的</div>
