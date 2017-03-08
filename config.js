f6d247207585bbaf17e0a57e6c99566fc3a01cca
MrOrz <johnsonliang7@gmail.com>
2017-03-08 23:51:03 +0200

    Avoid using ‘RUMORS_SITE’ app_id if we do not use local API server

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
   API_URL: process.env.USE_LOCAL ? 'http://localhost:5000' : 'http://api.rumors.hacktabl.org',
+  APP_ID: process.env.USE_LOCAL ? 'RUMORS_SITE' : 'DEV',
 };
--- util/fetchAPI.js
+++ util/fetchAPI.js
@@ -1,12 +1,12 @@
 import fetch from 'isomorphic-fetch';
-import {API_URL} from '../config';
+import {API_URL, APP_ID} from '../config';
 
 export default function(endpoint = '/', args = {}) {
   return fetch(`${API_URL}${endpoint}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'x-app-id': 'RUMORS_SITE',
+      'x-app-id': APP_ID,
       ...args.headers,
     },
     credentials: 'include',
