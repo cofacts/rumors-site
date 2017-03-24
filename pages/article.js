9685c4e879744ad5e7f421ed40d1de45a30ebbe1
MrOrz <johnsonliang7@gmail.com>
2017-03-25 02:08:17 +0800

    Update copy for RUMOR and NOT_RUMOR

--- pages/article.js
+++ pages/article.js
@@ -9,13 +9,13 @@ import { load, submitReply, connectReply } from '../redux/articleDetail';
 
 const TYPE_DESC = {
   NOT_ARTICLE: '這篇不是轉傳訊息或完整網路文章，如「這個要怎麼用」、或訊息的節錄片段。',
-  NOT_RUMOR: '這篇確實是轉傳訊息或網路文章，但屬於心得感想、新知分享等，沒有不實資訊。',
-  RUMOR: '含有內有不實資訊的轉傳訊息或網路文章。',
-};
+  NOT_RUMOR: '轉傳訊息或網路文章有一部分內容查證屬實。',
+  RUMOR: '轉傳訊息或網路文章部分含有有一資訊。',
+}
 
 const TYPE_INSTRUCTION = {
   NOT_ARTICLE: '請簡單說明您為何認為這不是完整文章：',
-  NOT_RUMOR: '請簡單說明為何您認為它沒有不實資訊：',
+  NOT_RUMOR: '請簡單說明他哪個部分是正確的，作為「資料來源」的導讀：',
   RUMOR: '請簡單說明不實之處，作為「資料來源」的導讀：',
 }
 
@@ -237,7 +237,7 @@ class ReplyForm extends React.PureComponent {
         <p>
           <select name="type" defaultValue="NOT_ARTICLE" onChange={this.handleTypeChange}>
             <option value="NOT_ARTICLE">⚠️️ 非完整文章或訊息</option>
-            <option value="NOT_RUMOR">⭕ 查無不實</option>
+            <option value="NOT_RUMOR">⭕ 含有正確訊息</option>
             <option value="RUMOR">❗ 含有不實訊息</option>
           </select>
           <span>：{TYPE_DESC[replyType]}</span>
