d6668be2fbaff2b45241e667bcacad8701db8bc5
MrOrz <johnsonliang7@gmail.com>
2017-06-12 00:22:29 +0800

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
+  parser: "babel-eslint",
   extends: 'react-app',
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+  ],
+  plugins: [
+    "react",
+    "prettier",
+  ],
+  rules: {
+    'react/prop-types': 'off', // we don't use propTypes.
+    'prettier/prettier': ["error", {
+      "trailingComma": "es5",
+      "singleQuote": true,
+    }],
+    'no-console': ['error', { allow: ['warn', 'error'] }], // no console.log, but can use .warn and .error.
+  },
+  env: {
+    browser: true,
+    mocha: false,
+    es6: true,
+    jest: true,
+    node: true,
+  },
 };
