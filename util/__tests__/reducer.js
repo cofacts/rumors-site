28d2b77f1967df77750093715116705e8f1f9275
MrOrz <johnsonliang7@gmail.com>
2017-10-03 02:27:51 +0800

    Add simple unit tests for reducer.js

--- util/__tests__/reducer.js
+++ util/__tests__/reducer.js
@@ -1,5 +1,25 @@
+import { fromJS } from 'immutable';
+import { commonSetState } from '../reducer';
+
 describe('reducer', () => {
   describe('commonSetState', () => {
-    it('sets state from payload');
+    const initState = fromJS({
+      state: {
+        foo: 'bar',
+      },
+    });
+    it('sets state from payload', () => {
+      expect(
+        commonSetState(initState, {
+          payload: { key: 'foo', value: 'bar2' },
+        }).get('state')
+      ).toMatchSnapshot();
+
+      expect(
+        commonSetState(initState, {
+          payload: { key: 'not-exist-key', value: 'new-value' },
+        }).get('state')
+      ).toMatchSnapshot();
+    });
   });
 });
