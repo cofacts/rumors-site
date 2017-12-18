5eee2308abdea03783508476fe95515f918b9035
Gore Wang <sunrise91.t3@gmail.com>
2017-12-18 21:06:55 +0800

    fix mobile url overflow by css word-break

--- components/ReplyConnection.styles.js
+++ components/ReplyConnection.styles.js
@@ -5,6 +5,7 @@ export const sectionStyle = css`
     padding-bottom: 8px;
     margin-bottom: 8px;
     border-bottom: 1px dotted rgba(0, 0, 0, 0.2);
+    word-break: break-all;
   }
   .section h3 {
     margin: 0;
