572a7021f42a02a78536f97e2b123984d784357c
MrOrz <johnsonliang7@gmail.com>
2018-02-10 01:47:54 +0800

    Add server-side rollbar configuration

--- server.js
+++ server.js
@@ -3,6 +3,14 @@
 
 const Koa = require('koa');
 const next = require('next');
+const Rollbar = require('rollbar');
+
+const rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_SERVER_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: process.env.ROLLBAR_ENV || 'localhost',
+});
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 
@@ -11,6 +19,16 @@ const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
   const server = new Koa();
+
+  // Rollbar error handling
+  //
+  server.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      rollbar.error(err, ctx.request);
+    }
+  });
 
   // Server-side routing
   //
