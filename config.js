7765f300081187290bb86f1e24e2f2843b254feb
MrOrz <johnsonliang7@gmail.com>
2018-02-17 19:56:03 +0800

    Introduce BUILD_TARGET and rewrite config.js
    In order to support “staging” environment. “USE_LOCAL” is replaced with “BUILD_TARGET”.

--- config.js
+++ config.js
@@ -1,17 +1,43 @@
+/**
+ * Configs used in both clients and the server.
+ * Please DON'T PUT ANY SERVER CREDENTIALS here because it's going to be bundled in client JS.
+ */
+
 import { config } from './package.json';
 
-module.exports = {
-  API_URL: process.env.USE_LOCAL
-    ? 'http://localhost:5000'
-    : 'https://cofacts-api.g0v.tw',
-
-  // If is dev but connet to production api, don't use appId = RUMORS_SITE.
-  // Use RUMORS_SITE only on production, or when connecting to local dev server
-  //
-  APP_ID:
-    process.env.USE_LOCAL || process.env.NODE_ENV === 'production'
-      ? 'RUMORS_SITE'
-      : 'DEV',
-  GA_TRACKER: 'UA-98468513-1',
+const common = {
   AUTOTRACK_FILENAME: config.autotrackFileName,
+  API_URL: 'https://cofacts-api.hacktabl.org', // Default to staging
+  APP_ID: 'DEV',
 };
+
+switch (process.env.BUILD_TARGET) {
+  case 'production':
+    module.exports = {
+      ...common,
+      API_URL: 'https://cofacts-api.g0v.tw',
+      APP_ID: 'RUMORS_SITE',
+      GA_TRACKER: 'UA-98468513-1',
+    };
+    break;
+
+  case 'staging':
+    module.exports = {
+      ...common,
+      API_URL: 'https://cofacts-api.hacktabl.org',
+      APP_ID: 'RUMORS_SITE',
+      GA_TRACKER: 'UA-98468513-5',
+    };
+    break;
+
+  case 'local':
+    // Own API server started using docker-compose in https://github.com/MrOrz/rumors-api
+    module.exports = {
+      ...common,
+      API_URL: 'http://localhost:5000',
+    };
+    break;
+
+  default:
+    module.exports = common;
+}
