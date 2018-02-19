c47f113c4ea50a62825f52f756943def0054d799
MrOrz <johnsonliang7@gmail.com>
2018-02-19 20:50:58 +0800

    Avoid using "replyConnections" field and query its ID

--- ducks/replyDetail.js
+++ ducks/replyDetail.js
@@ -32,7 +32,6 @@ export const load = id => dispatch => {
           createdAt
         }
         replyConnections: articleReplies {
-          id
           articleId
           article {
             id
