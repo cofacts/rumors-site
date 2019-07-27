1440bafa05de2e6d83f65e110492da9ace839713
MrOrz <johnsonliang7@gmail.com>
2019-07-27 22:13:30 +0800

    Add style to test FOUC

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-  return <div>Welcome to Next.js!</div>;
+  return <div>
+    Welcome to Next.js!
+    <style jsx>{`
+      div {
+        color: red;
+      }
+    `}</style>
+  </div>;
 }
 
 export default Home;
