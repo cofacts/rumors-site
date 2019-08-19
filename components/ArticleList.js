0eec98d2778254d3260ef402d224a337b444eba7
Johnson Liang <johnson.liang@appier.com>
2019-08-19 14:31:20 +0800

    Add immer and load-in-browser effect which does not update UI for some reason...

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,26 +1,61 @@
+import { useEffect } from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
+import produce from 'immer';
 
 const LIST_ARTICLE_QUERY = gql`
-  {
-  ListArticles(orderBy: [{createdAt: DESC}]) {
-    edges {
-      node {
-        id
-        text
+  query Articles {
+    ListArticles(orderBy: [{createdAt: DESC}]) {
+      edges {
+        node {
+          id
+          text
+        }
+        cursor
       }
-      cursor
-    }
-    pageInfo {
-      firstCursor
-      lastCursor
+      pageInfo {
+        firstCursor
+        lastCursor
+      }
     }
   }
-}
 `
 
+const LIST_ARTICLE_ON_LOAD = gql`
+  query ArticleUsers {
+    ListArticles(orderBy: [{createdAt: DESC}]) {
+      edges {
+        node {
+          user {
+            id, name
+          }
+        }
+      }
+    }
+  }
+`
+
+const updateArticlesOnLoad = produce((articlesQuery, articleUsersQuery) => {
+  const articleEdges = articleUsersQuery.ListArticles.edges;
+  articlesQuery.ListArticles.edges.forEach((edge, idx) => {
+    edge.node = {...edge.node, ...articleEdges[idx].node}
+  })
+})
+
 function ArticleList() {
-  const {loading, data} = useQuery(LIST_ARTICLE_QUERY);
+  const {loading, data, fetchMore} = useQuery(LIST_ARTICLE_QUERY);
+
+  useEffect(() => {
+    fetchMore({
+      query: LIST_ARTICLE_ON_LOAD,
+      updateQuery( prev, {fetchMoreResult}) {
+        if(!fetchMoreResult) return prev;
+        const updated = updateArticlesOnLoad(prev, fetchMoreResult);
+        console.log(updated, updated === prev);
+        return updated;
+      }
+    });
+  }, [])
 
   return loading ? (
     <p>Loading</p>
