3b19ef3ec90a572b49d361e1f3a307618d9130d9
MrOrz <johnsonliang7@gmail.com>
2019-08-21 03:36:26 +0800

    [pages/index] MUI components don't blink

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import ArticleList from "../components/ArticleList";
 
 function Home() {
   return <div>
-    Welcome to Next.js! { new Date().toLocaleString() }
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          Cofacts
+        </Typography>
+      </Toolbar>
+    </AppBar>
 
     <ArticleList />
     <style jsx>{`
