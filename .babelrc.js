1fcf7a62342023a9fb4975eedf2ce82fad7aa752
MrOrz <johnsonliang7@gmail.com>
2019-08-28 13:52:52 +0800

    Add eslint and babel module resolver

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   "presets": ["next/babel"],
-  "plugins": [[
-    'ttag', {resolve: {translations: `i18n/${process.env.PUBLIC_LOCALE}.po`}}
-  ]]
+  "plugins": [
+    [
+      "module-resolver",
+      // https://github.com/tleunen/babel-plugin-module-resolver#getting-started
+      {
+        "root": ["./"],
+        "alias": {
+          "components": "./components",
+          "constants": "./constants",
+          "pages": "./pages",
+        }
+      }
+    ], [
+      'ttag', {resolve: {translations: `i18n/${process.env.PUBLIC_LOCALE}.po`}}
+    ]
+  ]
 }
---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+  ],
+  plugins: [
+    "react",
+    "prettier",
+  ],
+  rules: {
+    'react/prop-types': 'off', // we don't use propTypes.
+    'react/react-in-jsx-scope': 'off', // React import not needed in Next.js
+    'prettier/prettier': ["error", {
+      "trailingComma": "es5",
+      "singleQuote": true,
+    }],
+    'no-console': ['error', { allow: ['warn', 'error'] }], // no console.log, but can use .warn and .error.
+  },
+  env: {
+    browser: true,
+    mocha: false,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {}
+    }
+  }
+};
