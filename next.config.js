2f6281dfaec0b44bc9d680b664e741873636427e
Johnson Liang <johnson.liang@appier.com>
2019-10-29 15:26:34 +0800

    Avoid using dotenv result because built images don't have .env it will cause empty "env" key passed to DefinePlugin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const { parsed } = require('dotenv').config();
+require('dotenv').config();
 
 const env = {};
 const publicRuntimeConfig = {};
 const serverRuntimeConfig = {};
 
-Object.keys(parsed || {}).forEach(key => {
+Object.keys(process.env || {}).forEach(key => {
   switch (true) {
     case key.startsWith('SERVER_'):
       serverRuntimeConfig[key] = process.env[key];
@@ -12,9 +12,19 @@ Object.keys(parsed || {}).forEach(key => {
     case key.startsWith('PUBLIC_'):
       publicRuntimeConfig[key] = process.env[key];
       break;
+    case key.startsWith('NODE_'):
+    case key.startsWith('__'):
+      // https://github.com/zeit/next.js/blob/master/errors/env-key-not-allowed.md
+      break;
     default:
       env[key] = process.env[key];
   }
+});
+
+console.log({
+  env,
+  publicRuntimeConfig,
+  serverRuntimeConfig,
 });
 
 module.exports = {
