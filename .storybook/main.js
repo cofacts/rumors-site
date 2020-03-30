3914130255de502902fe35893fb435cbeb94e8c4
Yang Lin <yanglin5689446@gmail.com>
2020-03-30 15:57:10 +0800

    Remove redundant storyshot plugin configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../components/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/storyshots'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
