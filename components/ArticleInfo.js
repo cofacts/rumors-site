2c9ce4f0578ab8a81b4f98cb99abb784dfc24c44
Yang Lin <yanglin5689446@gmail.com>
2020-04-13 02:36:14 +0800

    ArticleInfo

--- components/ArticleInfo.js
+++ components/ArticleInfo.js
@@ -1,43 +1,90 @@
 import gql from 'graphql-tag';
 import { t, ngettext, msgid } from 'ttag';
+import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import isValid from 'date-fns/isValid';
 import { format, formatDistanceToNow } from 'lib/dateWithLocale';
+import { TYPE_ICON } from 'constants/replyType';
 
+const useStyles = makeStyles(theme => ({
+  info: {
+    color: theme.palette.secondary[200],
+    '&:not(:first-child)': {
+      marginLeft: 6,
+      paddingLeft: 6,
+      borderLeft: `1px solid ${theme.palette.secondary[200]}`,
+    },
+  },
+  opinion: {
+    '&:not(:first-child)': {
+      paddingLeft: 15,
+    },
+  },
+}));
+
+const CustomTooltip = withStyles(theme => ({
+  arrow: {
+    color: theme.palette.secondary[500],
+  },
+  tooltip: {
+    backgroundColor: theme.palette.secondary[500],
+  },
+}))(Tooltip);
 export default function ArticleInfo({ article }) {
   const createdAt = new Date(article.createdAt);
   const { replyRequestCount, replyCount } = article;
   const timeAgoStr = formatDistanceToNow(createdAt);
 
+  const opinions = (
+    article?.articleReplies.map(({ reply }) => reply.type) || []
+  ).reduce((result, current) => {
+    if (result[current]) {
+      result[current] += 1;
+    } else {
+      result[current] = 1;
+    }
+    return result;
+  }, {});
+
+  const classes = useStyles();
+
   return (
-    <div className="root">
-      {ngettext(
-        msgid`${replyRequestCount} occurence`,
-        `${replyRequestCount} occurences`,
-        replyRequestCount
+    <div>
+      <span className={classes.info}>
+        {ngettext(
+          msgid`${replyRequestCount} occurence`,
+          `${replyRequestCount} occurences`,
+          replyRequestCount
+        )}
+      </span>
+
+      {article.replyCount > 0 && (
+        <CustomTooltip
+          title={
+            <>
+              {Object.entries(opinions).map(([k, v]) => (
+                <span key={k} className={classes.opinion}>
+                  {TYPE_ICON[k]} {v}
+                </span>
+              ))}
+            </>
+          }
+          arrow
+        >
+          <span className={classes.info}>
+            {ngettext(
+              msgid`${replyCount} response`,
+              `${replyCount} responses`,
+              replyCount
+            )}
+          </span>
+        </CustomTooltip>
       )}
-      {article.replyCount > 0 ? (
-        <span>
-          {' | '}
-          {ngettext(
-            msgid`${replyCount} response`,
-            `${replyCount} responses`,
-            replyCount
-          )}
-        </span>
-      ) : (
-        ''
+      {isValid(createdAt) && (
+        <CustomTooltip title={format(createdAt)} arrow>
+          <span className={classes.info}>{t`${timeAgoStr} ago`}</span>
+        </CustomTooltip>
       )}
-      {isValid(createdAt) ? (
-        <span title={format(createdAt)}> | {t`${timeAgoStr} ago`}</span>
-      ) : (
-        ''
-      )}
-      <style jsx>{`
-        .root {
-          font-size: var(--font-size);
-          color: rgba(0, 0, 0, 0.5);
-        }
-      `}</style>
     </div>
   );
 }
