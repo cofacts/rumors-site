e54a69afd8cd3a0148be2714b5fe4b81f871d8ac
MrOrz <johnsonliang7@gmail.com>
2020-05-18 15:12:57 +0800

    Track Timerange option click

--- components/TimeRange.js
+++ components/TimeRange.js
@@ -181,7 +181,11 @@ function TimeRange({ onChange = () => null, range }) {
         onClose={closeMenu}
       >
         {options.map(option => (
-          <MenuItem key={option.value} onClick={select(option.value)}>
+          <MenuItem
+            key={option.value}
+            onClick={select(option.value)}
+            data-ga={`MenuItem(${option.value})`}
+          >
             {option.label}
           </MenuItem>
         ))}
