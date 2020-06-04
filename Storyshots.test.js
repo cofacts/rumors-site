f79975c0d54d742cd1edbbf3e24e46737ce52c01
MrOrz <johnsonliang7@gmail.com>
2020-06-04 21:11:29 +0800

    [WIP] storyshot for TimeRange

--- Storyshots.test.js
+++ Storyshots.test.js
@@ -1,3 +1,11 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { createMount, createRender } from '@material-ui/core/test-utils';
 import initStoryshots from '@storybook/addon-storyshots';
 
-initStoryshots();
+Enzyme.configure({ adapter: new Adapter() });
+
+initStoryshots({
+  renderer: createMount(),
+  // snapshotSerializers: [createRender()],
+});
