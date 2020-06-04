3cee465b8ae2a266f1a434bd54f4df9b21fc2e26
MrOrz <johnsonliang7@gmail.com>
2020-06-05 00:04:47 +0800

    Add comment about jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   transform: {
+    // Required by Storyshot multiSnapshotWithOptions()
+    // Ref: https://github.com/storybookjs/storybook/tree/master/addons/storyshots/storyshots-core#multisnapshotwithoptionsoptions
+    //
     '^.+\\.stories\\.jsx?$': '@storybook/addon-storyshots/injectFileName',
     '^.+\\.jsx?$': 'babel-jest',
   },
