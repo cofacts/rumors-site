839f59b4c61f278829cb0b8734ba6c6538941a6b
MrOrz <johnsonliang7@gmail.com>
2020-06-04 23:34:00 +0800

    Remove irrelevant parts of snapshot

--- Storyshots.test.js
+++ Storyshots.test.js
@@ -5,7 +5,35 @@ import { createSerializer } from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+function removeMaterialUIInternals(json) {
+  // Remove Portal containerInfo
+  if (json.type === 'Portal') {
+    return {
+      ...json,
+      props: {
+        ...json.props,
+        containerInfo: undefined,
+      },
+    };
+  }
+
+  // Remove all props of these components
+  if (['Transition', 'TrapFocus'].includes(json.type)) {
+    return {
+      ...json,
+      props: {},
+    };
+  }
+
+  // Skip HOC components (single children)
+  if (json.type.match(/^(ForwardRef|WithStyles)/)) {
+    return json.children && json.children[0];
+  }
+
+  return json;
+}
+
 initStoryshots({
   renderer: mount,
-  snapshotSerializers: [createSerializer()],
+  snapshotSerializers: [createSerializer({ map: removeMaterialUIInternals })],
 });
