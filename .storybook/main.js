1a52ac45977444fb284d1dac68e9e80177c7e84c
MrOrz <johnsonliang7@gmail.com>
2020-06-27 18:08:01 +0800

    Global style definition for website & storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
-  stories: ['../components/*.stories.js'],
-  addons: ['@storybook/addon-actions/register', '@storybook/addon-knobs/register'],
+  stories: ['../components/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+  ],
 };
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,9 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../lib/theme';
 
+// https://nextjs.org/docs/basic-features/built-in-css-support
+import '../components/app.css';
+
 class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
