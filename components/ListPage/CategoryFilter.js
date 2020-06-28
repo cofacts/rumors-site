d5b1dfdb85ef4af7e686e2a7d721a0e9e3f5426a
MrOrz <johnsonliang7@gmail.com>
2020-06-28 19:44:08 +0800

    Fix CategoryFilter

--- components/ListPage/CategoryFilter.js
+++ components/ListPage/CategoryFilter.js
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { useRouter } from 'next/router';
 import { t } from 'ttag';
 import gql from 'graphql-tag';
-import { useLazyQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 
 import { goToUrlQueryAndResetPagination } from 'lib/listPage';
 import BaseFilter from './BaseFilter';
@@ -23,7 +23,9 @@ const LIST_CATEGORIES = gql`
 
 function CategoryFilter() {
   const { query } = useRouter();
-  const { data } = useLazyQuery(LIST_CATEGORIES); // No need to fetch on server
+  const { data } = useQuery(LIST_CATEGORIES, {
+    ssr: false, // No need to fetch on server
+  });
 
   const selectedValues = query.categoryIds ? query.categoryIds.split(',') : [];
   const options =
