f084110b30f6bc4c42973cbf2183bf2e11513977
Zoe <1817638+ztsai@users.noreply.github.com>
2020-08-19 19:02:51 +0800

    hitbox

--- components/LineChart.js
+++ components/LineChart.js
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
 import { makeStyles } from '@material-ui/core/styles';
-
+const ONEDAY = 86400000;
 const useStyles = makeStyles(theme => {
   return {
     plot: {
@@ -22,39 +22,71 @@ const useStyles = makeStyles(theme => {
         stroke: theme.palette.primary.main,
       },
       '& .webDot': {
-        fill: theme.palette.info.main,
+        fill: theme.palette.secondary[300],
+        opcaity: 0.5
       },
       '& .chatbotDot': {
-        fill: theme.palette.primary.main,
+        fill: theme.palette.secondary[300],
+        opcaity: 0.5
       },
     },
+    wrapper: {
+      display: 'block',
+      position: 'absolute'
+    },
+    hitbox: {
+      display: 'none',
+      position: 'absolute',
+      background: 'rgba(200, 200,200,0.5)',
+      '&:hover': {
+        display: 'inline-block'
+      }
+    },
+    vLine: {
+      width: '1px',
+      border: '1px solid rgba(0,0,0,0.1)',
+      position: 'absolute'
+    },
+    dot: {
+      width: '10px',
+      height: '10px',
+      borderRadius: '5px',
+      position: 'absolute',
+
+      '&.web': {
+        background: theme.palette.info.main,
+      },
+      '&.chatbot': {
+        background: theme.palette.primary.main,
+      }
+
+    }
   };
 });
 
 /**
  * Given analytics stat, performs computations needed to plot a line chart.
 
- * @param {array}  data    Array of Analytics objects as defined in graphql/models/Analytics.
- * @param {number} width   Width of plot area.
- * @param {number} height  Height of plot area.
+ * @param {array}  dataset List of data of the form {date, webVisit, lineVisit}
+ * @param {number} width   Width of plot area
+ * @param {number} height  Height of plot area
 
  * @return {
-    dataset:     {array}             List of data of the form {date, webVisit, lineVisit}.
     xScale:      {d3.scaleTime}
     yScaleWeb:   {d3.scaleLinear}
     yScaleLine:  {d3.scaleLinear}
-    chatbotDots: {array}             List of corresponding positions for each chatbot entry.
+    chatbotDots: {array}             List of corresponding positions for each chatbot entry
     chatbotLine: {d3.line}
-    webDots:     {array}             List of corresponding positions for each web entry.
+    webDots:     {array}             List of corresponding positions for each web entry
     webLine:     {d3.line}
  }
  */
-const getChartData = (data, width, height) => {
-  const dataset = data.map(({ date, webVisit, lineVisit }) => ({
+const computeChartData = (dataset, width, height) => {
+  /*const dataset = data.map(({ date, webVisit, lineVisit }) => ({
     date: new Date(date),
     webVisit: webVisit ? +webVisit : 0,
     lineVisit: lineVisit ? +lineVisit : 0,
-  }));
+  }));*/
 
   // round up to the nearest 10
   const maxWebVisit = Math.ceil(d3.max(dataset, d => d.webVisit) / 10) * 10;
@@ -87,13 +119,15 @@ const getChartData = (data, width, height) => {
     .y(d => yScaleLine(d.lineVisit))
     .curve(d3.curveMonotoneX);
 
+
+
+
   const webDots = dataset.map(d => ({
     value: d.webVisit,
     date: d.date,
     cx: xScale(d.date),
     cy: yScaleWeb(d.webVisit),
   }));
-
   const chatbotDots = dataset.map(d => ({
     value: d.lineVisit,
     date: d.date,
@@ -102,7 +136,6 @@ const getChartData = (data, width, height) => {
   }));
 
   return {
-    dataset,
     xScale,
     yScaleWeb,
     yScaleLine,
@@ -125,9 +158,10 @@ function TickGroup({ x, y, gridline, text }) {
   );
 }
 /* Populates a list of tick values given a scale and number of ticks wanted. */
-const getTicks = (scale, tickNum) => {
-  const [min, max] = scale.domain();
-  const diff = (max - min) / (tickNum - 1);
+const getTicks = (scale, roundFn, tickNum) => {
+  const [max, min] = scale.domain();
+  debugger
+  const diff = roundFn((max - min) / (tickNum - 1));
   let ticks = [];
   for (let i = 0; i < tickNum; i++) {
     ticks.push(max - diff * i);
@@ -140,8 +174,8 @@ const formatDate = date => {
   return `${d.getMonth() + 1}/${d.getDate()}`; // MM/DD
 };
 
-function plotTicks({ scale, x, y, text, tickNum, gridline }) {
-  const ticks = getTicks(scale, tickNum);
+function plotTicks({ scale, x, y, text, roundFn, tickNum, gridline }) {
+  const ticks = getTicks(scale, roundFn, tickNum);
   return ticks.map(tick => (
     <TickGroup
       key={tick}
@@ -159,13 +193,15 @@ function plotDots(dots, className) {
   ));
 }
 
-export default function LineChart({ data, layout: { width, height, margin } }) {
+export default function LineChart({ dataset, layout: { width, height, margin } }) {
   const classes = useStyles();
 
   const innerWidth = width - margin.left - margin.right;
   const innerHeight = height - margin.top - margin.bottom;
 
-  const chartData = getChartData(data, innerWidth, innerHeight);
+  const boxWidth = innerWidth / (dataset.length - 1);
+
+  const chartData = computeChartData(dataset, innerWidth, innerHeight);
   return (
     <div>
       <svg width={width} height={height}>
@@ -173,12 +209,14 @@ export default function LineChart({ data, layout: { width, height, margin } }) {
           className={classes.plot}
           transform={`translate(${margin.left}, ${margin.top})`}
         >
+          <line class="gridline" y2={innerHeight} transform={`translate(${innerWidth},0)`}/>
           <g className="leftAxis" textAnchor="end">
             {plotTicks({
               scale: chartData.yScaleWeb,
               tickNum: 3,
               x: () => 0,
               y: (scale, tick) => scale(tick),
+              roundFn: value => Math.ceil(value),
               text: tick => ({ x: -9, y: 5, text: tick }),
               gridline: { x2: innerWidth },
             })}
@@ -194,6 +232,7 @@ export default function LineChart({ data, layout: { width, height, margin } }) {
               tickNum: 3,
               x: () => 0,
               y: (scale, tick) => scale(tick),
+              roundFn: value => Math.ceil(value),
               text: tick => ({ x: 8, y: 5, text: tick }),
               gridline: {},
             })}
@@ -209,6 +248,7 @@ export default function LineChart({ data, layout: { width, height, margin } }) {
               tickNum: 10,
               x: (scale, tick) => scale(tick),
               y: () => 0,
+              roundFn: value => (Math.ceil(value/ONEDAY)*ONEDAY),
               text: tick => ({ x: 0, y: 20, text: formatDate(tick) }),
               gridline: { y2: -innerHeight },
             })}
@@ -217,19 +257,28 @@ export default function LineChart({ data, layout: { width, height, margin } }) {
           <g className="webGroup">
             <path
               className="line webLine"
-              d={chartData.webLine(chartData.dataset)}
+              d={chartData.webLine(dataset)}
             />
             {plotDots(chartData.webDots, 'webDot')}
           </g>
           <g className="chatbotGroup">
             <path
               className="line chatbotLine"
-              d={chartData.chatbotLine(chartData.dataset)}
+              d={chartData.chatbotLine(dataset)}
             />
             {plotDots(chartData.chatbotDots, 'chatbotDot')}
           </g>
         </g>
       </svg>
+      <div className={classes.wrapper} style={{left: margin.left + 'px', top: margin.top + 'px'}}>
+        {dataset.map((d, i)=>
+          <div className={classes.hitbox} key={i} style={{left: boxWidth * (i - 0.5) + 'px', width:boxWidth+'px', height:innerHeight+'px'}}>
+            <div className={`${classes.dot} web`} style={{top: chartData.yScaleWeb(d.webVisit) - 5 + 'px', left: boxWidth/2 - 5+'px'}}></div>
+            <div className={`${classes.dot} chatbot`} style={{top: chartData.yScaleLine(d.lineVisit) - 5 + 'px', left: boxWidth/2 - 5+'px'}}></div>
+            <div className={`${classes.vLine}`} style={{height:innerHeight+'px', left: boxWidth/2+'px'}}></div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
