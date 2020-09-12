ad464e68170fa0da388fb0a7b498b9ff859cf1ed
MrOrz <johnsonliang7@gmail.com>
2020-09-12 12:17:33 +0800

    [ListPageDisplays] ListPageHeader font size and layout match mockup

--- components/ListPageDisplays/ListPageHeader.js
+++ components/ListPageDisplays/ListPageHeader.js
@@ -1,19 +1,36 @@
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import cx from 'clsx';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    margin: '24px 0 20px',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    '& > h1': {
+      fontSize: 18,
+      fontWeight: 500,
+      margin: '0 22px 0 0',
+    },
+
+    [theme.breakpoints.up('md')]: {
+      margin: '48px 0 24px',
+      '& > h1': {
+        fontSize: 34,
+      },
+      justifyContent: 'space-between',
+    },
+  },
+}));
 
 function ListPageHeader({ title, children, className }) {
+  const classes = useStyles();
   return (
-    <Box
-      className={className}
-      display="flex"
-      alignItems="center"
-      justifyContent={{ xs: 'center', md: 'space-between' }}
-      flexDirection={{ xs: 'column', md: 'row' }}
-      mb={2}
-    >
-      <Typography variant="h4">{title}</Typography>
-      <Box my={1}>{children}</Box>
-    </Box>
+    <header className={cx(className, classes.root)}>
+      <h1>{title}</h1>
+      {children}
+    </header>
   );
 }
 
