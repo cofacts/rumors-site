81b4dc97b32a58e79a3379d5c47c506f36e7c5fc
MrOrz <johnsonliang7@gmail.com>
2020-09-13 12:09:01 +0800

    [ListPageControls][ListPageDisplays] make export shorter

---
+++ components/ListPageDisplays/index.js
@@ -0,0 +1,6 @@
+import ListPageCards from './ListPageCards';
+import ArticleCard from './ArticleCard';
+import ReplyItem from './ReplyItem';
+import ListPageHeader from './ListPageHeader';
+
+export { ListPageCards, ArticleCard, ReplyItem, ListPageHeader };
