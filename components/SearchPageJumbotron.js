d3cf75b03308c8e2ae2456554414b4a9bca1e68c
MrOrz <johnsonliang7@gmail.com>
2020-09-13 21:29:42 +0800

    [search] remove ArticlePageLayout and ReplySearchPageLayout

---
+++ components/SearchPageJumbotron.js
@@ -0,0 +1,138 @@
+import { useRef, useEffect, memo } from 'react';
+import { useRouter } from 'next/router';
+import { t } from 'ttag';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { Tabs, Tab, Box, Container } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles(theme => ({
+  jumbotron: {
+    background: '#202020',
+  },
+  search: {
+    color: theme.palette.common.white,
+    paddingRight: theme.spacing(3),
+  },
+  form: {
+    padding: '56px 0',
+    display: 'flex',
+    alignItems: 'baseline',
+  },
+  inputArea: {
+    position: 'relative',
+    background: theme.palette.secondary[400],
+    border: `1px dashed ${theme.palette.secondary[200]}`,
+    borderRadius: 8,
+    padding: theme.spacing(1),
+    '&:before, &:after': {
+      display: 'block',
+      position: 'absolute',
+      color: '#B9B9B9',
+      fontSize: '2rem',
+    },
+    '&:before': {
+      content: '"“"',
+      top: -theme.spacing(1),
+      left: -theme.spacing(2),
+    },
+    '&:after': {
+      content: '"”"',
+      bottom: -theme.spacing(3),
+      right: -theme.spacing(2),
+    },
+    '&:focus-within': {
+      border: `1px solid ${theme.palette.primary[500]}`,
+      '& $submit': {
+        display: 'block',
+      },
+    },
+  },
+  input: {
+    width: '100%',
+    border: 'none',
+    outline: 'none',
+    color: theme.palette.common.white,
+    background: 'transparent',
+  },
+  submit: {
+    display: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+    height: '100%',
+    position: 'absolute',
+    border: `1px solid ${theme.palette.primary[500]}`,
+    top: 0,
+    right: 0,
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+    background: theme.palette.primary[500],
+    color: theme.palette.common.white,
+    '& > svg': {
+      verticalAlign: 'middle',
+    },
+  },
+}));
+
+const CustomTab = withStyles(theme => ({
+  root: {
+    color: theme.palette.common.white,
+    fontSize: theme.typography.htmlFontSize,
+  },
+}))(Tab);
+
+function SearchPageJumbotron() {
+  const router = useRouter();
+  const { query } = router;
+  const textareaRef = useRef(null);
+
+  const classes = useStyles();
+
+  const onSearch = e => {
+    e.preventDefault();
+    router.push({
+      pathname: '/search',
+      query: { ...query, q: e.target.search.value },
+    });
+  };
+
+  const { q } = query;
+  useEffect(() => {
+    textareaRef.current.value = q;
+  }, [q]);
+
+  const navigate = type =>
+    router.push({ pathname: '/search', query: { ...query, type } });
+
+  return (
+    <div className={classes.jumbotron}>
+      <Container>
+        <form onSubmit={onSearch} className={classes.form}>
+          <h2 className={classes.search}>{t`Searching`}</h2>
+          <Box flex={1} className={classes.inputArea}>
+            <textarea
+              ref={textareaRef}
+              name="search"
+              className={classes.input}
+              rows={1}
+            />
+            <button type="submit" className={classes.submit}>
+              <SearchIcon />
+            </button>
+          </Box>
+        </form>
+        <Tabs
+          value={query.type}
+          onChange={(e, page) => navigate(page)}
+          indicatorColor="primary"
+          textColor="primary"
+          aria-label="tabs"
+        >
+          <CustomTab value="messages" label={t`Messages`} />
+          <CustomTab value="replies" label={t`Replies`} />
+        </Tabs>
+      </Container>
+    </div>
+  );
+}
+
+export default memo(SearchPageJumbotron);
--- pages/search.js
+++ pages/search.js
@@ -1,180 +1,324 @@
-import { useRef, useEffect } from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import { Tabs, Tab, Box, Container } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+import gql from 'graphql-tag';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import getConfig from 'next/config';
 import { t } from 'ttag';
-import querystring from 'querystring';
-import ArticlePageLayout from 'components/ArticlePageLayout';
-import ReplySearchPageLayout from 'components/ReplySearchPageLayout';
+import { useQuery } from '@apollo/react-hooks';
+
+import Box from '@material-ui/core/Box';
+import { Container } from '@material-ui/core';
+import {
+  ListPageCards,
+  ArticleCard,
+  ReplySearchItem,
+} from 'components/ListPageDisplays';
+import {
+  Tools,
+  Filters,
+  BaseFilter,
+  CategoryFilter,
+  ReplyTypeFilter,
+  TimeRange,
+  LoadMore,
+} from 'components/ListPageControls';
 import AppLayout from 'components/AppLayout';
+import SearchPageJumbotron from 'components/SearchPageJumbotron';
 import withData from 'lib/apollo';
+import useCurrentUser from 'lib/useCurrentUser';
 
-const {
-  publicRuntimeConfig: { PUBLIC_URL },
-} = getConfig();
+const MAX_KEYWORD_LENGTH = 500;
 
-const useStyles = makeStyles(theme => ({
-  jumbotron: {
-    position: 'absolute',
-    background: '#202020',
-    left: 0,
-    right: 0,
-  },
-  search: {
-    color: theme.palette.common.white,
-    paddingRight: theme.spacing(3),
-  },
-  content: {
-    paddingTop: 230,
-  },
-  form: {
-    padding: '56px 0',
-    display: 'flex',
-    alignItems: 'baseline',
-  },
-  inputArea: {
-    position: 'relative',
-    background: theme.palette.secondary[400],
-    border: `1px dashed ${theme.palette.secondary[200]}`,
-    borderRadius: 8,
-    padding: theme.spacing(1),
-    '&:before, &:after': {
-      display: 'block',
-      position: 'absolute',
-      color: '#B9B9B9',
-      fontSize: '2rem',
+const LIST_ARTICLES = gql`
+  query ArticleSearchResult($filter: ListArticleFilter, $after: String) {
+    ListArticles(
+      filter: $filter
+      orderBy: [{ _score: DESC }]
+      after: $after
+      first: 25
+    ) {
+      edges {
+        node {
+          id
+          replyRequestCount
+          createdAt
+          text
+          ...ArticleCard
+        }
+        cursor
+      }
+    }
+  }
+  ${ArticleCard.fragments.ArticleCard}
+`;
+
+const LIST_REPLIES = gql`
+  query ReplySearchResult($filter: ListReplyFilter, $after: String) {
+    ListReplies(
+      filter: $filter
+      orderBy: [{ _score: DESC }]
+      after: $after
+      first: 10
+    ) {
+      edges {
+        node {
+          ...ReplySearchItem
+        }
+        cursor
+      }
+    }
+  }
+  ${ReplySearchItem.fragments.ReplySearchItem}
+`;
+
+const LIST_ARTICLE_STAT = gql`
+  query ArticleSearchResultStat($filter: ListArticleFilter) {
+    ListArticles(filter: $filter, orderBy: [{ _score: DESC }], first: 25) {
+      pageInfo {
+        firstCursor
+        lastCursor
+      }
+      totalCount
+    }
+  }
+`;
+
+const LIST_REPLY_STAT = gql`
+  query ReplySearchResultStat($filter: ListReplyFilter) {
+    ListReplies(filter: $filter, orderBy: [{ _score: DESC }], first: 25) {
+      pageInfo {
+        firstCursor
+        lastCursor
+      }
+      totalCount
+    }
+  }
+`;
+
+/**
+ * Filters that is common across ListAriticleFilter and ListReplyFilter
+ *
+ * @param {object} query - URL query object
+ * @returns {object} ListArticleFilter & ListReplyFilter
+ */
+function urlQuery2Filter(query = {}) {
+  const filterObj = {
+    moreLikeThis: {
+      like: query.q.slice(0, MAX_KEYWORD_LENGTH),
+      minimumShouldMatch: '0',
     },
-    '&:before': {
-      content: '"“"',
-      top: -theme.spacing(1),
-      left: -theme.spacing(2),
-    },
-    '&:after': {
-      content: '"”"',
-      bottom: -theme.spacing(3),
-      right: -theme.spacing(2),
-    },
-    '&:focus-within': {
-      border: `1px solid ${theme.palette.primary[500]}`,
-      '& $submit': {
-        display: 'block',
-      },
-    },
-  },
-  input: {
-    width: '100%',
-    border: 'none',
-    outline: 'none',
-    color: theme.palette.common.white,
-    background: 'transparent',
-  },
-  submit: {
-    display: 'none',
-    outline: 'none',
-    cursor: 'pointer',
-    height: '100%',
-    position: 'absolute',
-    border: `1px solid ${theme.palette.primary[500]}`,
-    top: 0,
-    right: 0,
-    borderTopRightRadius: 8,
-    borderBottomRightRadius: 8,
-    background: theme.palette.primary[500],
-    color: theme.palette.common.white,
-    '& > svg': {
-      verticalAlign: 'middle',
-    },
-  },
-}));
-
-const CustomTab = withStyles(theme => ({
-  root: {
-    color: theme.palette.common.white,
-    fontSize: theme.typography.htmlFontSize,
-    '&$selected': {
-      color: theme.palette.primary,
-    },
-  },
-}))(Tab);
-
-function SearchPage() {
-  const router = useRouter();
-  const queryString = querystring.stringify(router.query);
-  const { query } = router;
-  const textareaRef = useRef(null);
-
-  const classes = useStyles();
-
-  const navigate = type =>
-    router.push({ pathname: '/search', query: { ...query, type } });
-
-  const onSearch = e => {
-    e.preventDefault();
-    router.push({
-      pathname: '/search',
-      query: { ...query, q: e.target.search.value },
-    });
   };
 
-  const { q } = query;
-  useEffect(() => {
-    textareaRef.current.value = q;
-  }, [q]);
+  const [start, end] = TimeRange.getValues(query);
+
+  if (start) {
+    filterObj.createdAt = { ...filterObj.createdAt, GTE: start };
+  }
+  if (end) {
+    filterObj.createdAt = { ...filterObj.createdAt, LTE: end };
+  }
+
+  // Return filterObj only when it is populated.
+  if (!Object.keys(filterObj).length) {
+    return undefined;
+  }
+
+  return filterObj;
+}
+
+/**
+ * Search for matching articles and list the result
+ *
+ * @param {object} props.query - URL param object
+ * @param {string?} props.userId - If given, list only articles replied by this user ID
+ */
+function MessageSearchResult({ query, userId }) {
+  const replyTypes = ReplyTypeFilter.getValues(query);
+  const listQueryVars = {
+    filter: {
+      ...urlQuery2Filter(query),
+      categoryIds: CategoryFilter.getValues(query),
+      ...(replyTypes.length ? { replyTypes } : {}),
+      ...(userId
+        ? {
+            articleRepliesFrom: {
+              userId,
+              exists: true,
+            },
+          }
+        : {}),
+    },
+  };
+
+  const {
+    loading,
+    fetchMore,
+    data: listArticlesData,
+    error: listArticlesError,
+  } = useQuery(LIST_ARTICLES, {
+    variables: listQueryVars,
+    notifyOnNetworkStatusChange: true, // Make loading true on `fetchMore`
+  });
+
+  // Separate these stats query so that it will be cached by apollo-client and sends no network request
+  // on page change, but still works when filter options are updated.
+  //
+  const { data: listStatData } = useQuery(LIST_ARTICLE_STAT, {
+    variables: listQueryVars,
+  });
+
+  // List data
+  const articleEdges = listArticlesData?.ListArticles?.edges || [];
+  const statsData = listStatData?.ListArticles || {};
+
+  if (loading && !articleEdges.length) return t`Loading...`;
+  if (listArticlesError) return listArticlesError.toString();
 
   return (
-    <AppLayout>
+    <>
+      <ListPageCards>
+        {articleEdges.map(({ node: article }) => (
+          <ArticleCard key={article.id} article={article} query={query.q} />
+        ))}
+      </ListPageCards>
+
+      <LoadMore
+        edges={articleEdges}
+        pageInfo={statsData?.pageInfo}
+        loading={loading}
+        onMoreRequest={args =>
+          fetchMore({
+            variables: args,
+            updateQuery(prev, { fetchMoreResult }) {
+              if (!fetchMoreResult) return prev;
+              const newArticleData = fetchMoreResult?.ListArticles;
+              return {
+                ...prev,
+                ListArticles: {
+                  ...newArticleData,
+                  edges: [...articleEdges, ...newArticleData.edges],
+                },
+              };
+            },
+          })
+        }
+      />
+    </>
+  );
+}
+
+/**
+ * Search for matching replies and list the result
+ *
+ * @param {object} props.query - URL param object
+ * @param {boolean} props.selfOnly - If true, list only replies written by current user
+ */
+function ReplySearchResult({ query, selfOnly }) {
+  const types = ReplyTypeFilter.getValues(query);
+  const listQueryVars = {
+    filter: {
+      ...urlQuery2Filter(query),
+      ...(types.length ? { types } : {}),
+      selfOnly,
+    },
+  };
+
+  const {
+    loading,
+    fetchMore,
+    data: listRepliesData,
+    error: listRepliesError,
+  } = useQuery(LIST_REPLIES, {
+    variables: listQueryVars,
+    notifyOnNetworkStatusChange: true, // Make loading true on `fetchMore`
+  });
+
+  // Separate these stats query so that it will be cached by apollo-client and sends no network request
+  // on page change, but still works when filter options are updated.
+  //
+  const { data: listStatData } = useQuery(LIST_REPLY_STAT, {
+    variables: listQueryVars,
+  });
+
+  // List data
+  const replyEdges = listRepliesData?.ListReplies?.edges || [];
+  const statsData = listStatData?.ListReplies || {};
+
+  if (loading && !replyEdges.length) return t`Loading...`;
+  if (listRepliesError) return listRepliesError.toString();
+
+  return (
+    <>
+      <Box component="ul" p={0}>
+        {replyEdges.map(({ node }) => (
+          <ReplySearchItem key={node.id} {...node} query={query.q} />
+        ))}
+      </Box>
+
+      <LoadMore
+        edges={replyEdges}
+        pageInfo={statsData?.pageInfo}
+        loading={loading}
+        onMoreRequest={args =>
+          fetchMore({
+            variables: args,
+            updateQuery(prev, { fetchMoreResult }) {
+              if (!fetchMoreResult) return prev;
+              const newData = fetchMoreResult?.ListReplies;
+              return {
+                ...prev,
+                ListReplies: {
+                  ...newData,
+                  edges: [...replyEdges, ...newData.edges],
+                },
+              };
+            },
+          })
+        }
+      />
+    </>
+  );
+}
+
+function SearchPage() {
+  const { query } = useRouter();
+  const user = useCurrentUser();
+  const [selfOnly, setSelfOnly] = useState(false);
+
+  return (
+    <AppLayout container={false}>
       <Head>
         <title>{t`Search`}</title>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          href={`${PUBLIC_URL}/api/articles/rss2?${queryString}`}
-        />
-        <link
-          rel="alternate"
-          type="application/atom+xml"
-          href={`${PUBLIC_URL}/api/articles/atom1?${queryString}`}
-        />
       </Head>
-      <div className={classes.jumbotron}>
-        <Container>
-          <form onSubmit={onSearch} className={classes.form}>
-            <h2 className={classes.search}>{t`Searching`}</h2>
-            <Box flex={1} className={classes.inputArea}>
-              <textarea
-                ref={textareaRef}
-                name="search"
-                className={classes.input}
-                rows={1}
-              />
-              <button type="submit" className={classes.submit}>
-                <SearchIcon />
-              </button>
-            </Box>
-          </form>
-        </Container>
-        <Container>
-          <Tabs
-            value={query.type}
-            onChange={(e, page) => navigate(page)}
-            indicatorColor="primary"
-            textColor="primary"
-            aria-label="tabs"
-          >
-            <CustomTab value="messages" label={t`Messages`} />
-            <CustomTab value="replies" label={t`Replies`} />
-          </Tabs>
-        </Container>
-      </div>
-      <div className={classes.content}>
+
+      <SearchPageJumbotron />
+
+      <Container>
+        <Tools>
+          <TimeRange />
+        </Tools>
+
+        <Filters>
+          <BaseFilter
+            title={t`Filter`}
+            options={[
+              { value: 'self', label: t`Replied by me`, disabled: !user },
+            ]}
+            onChange={values => setSelfOnly(values.include('self'))}
+          />
+          <ReplyTypeFilter />
+          {query.type === 'messages' && <CategoryFilter />}
+        </Filters>
+
         {query.type === 'messages' && (
-          <ArticlePageLayout page="search" displayHeader={false} />
+          <MessageSearchResult
+            query={query}
+            userId={selfOnly ? user?.id : undefined}
+          />
         )}
-        {query.type === 'replies' && <ReplySearchPageLayout />}
-      </div>
+        {query.type === 'replies' && (
+          <ReplySearchResult query={query} selfOnly={selfOnly} />
+        )}
+      </Container>
     </AppLayout>
   );
 }
