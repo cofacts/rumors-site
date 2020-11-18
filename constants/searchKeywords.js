a61308b88a2698c064d9b2e797cc1a5533d68dc6
yutunghsiao <yutunghsiao19@gmail.com>
2020-11-18 22:19:47 +0800

    [LandingPage] update search keywords

--- constants/searchKeywords.js
+++ constants/searchKeywords.js
@@ -1,6 +1,2 @@
-export const SEARCH_KEYWORDS_ZH = ['武漢肺炎', '總統府駭客', '數位諸葛亮'];
-export const SEARCH_KEYWORDS_EN = [
-  '武漢肺炎 en',
-  '總統府駭客 en',
-  '數位諸葛亮 en',
-];
+export const SEARCH_KEYWORDS_ZH = ['武漢肺炎', '輕鬆賺錢', '抗癌'];
+export const SEARCH_KEYWORDS_EN = ['COVID-19', 'Donald Trump', 'China'];
