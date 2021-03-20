a30088279de0677cb1797e6ebc4cf132a6033397
Kerrick Staley <kerrick@kerrickstaley.com>
2021-03-20 13:00:10 +0800

    Define + use formatTimeInfoDate function

--- components/Infos/TimeInfo.js
+++ components/Infos/TimeInfo.js
@@ -3,6 +3,29 @@ import isValid from 'date-fns/isValid';
 import { format, formatDistanceToNow } from 'lib/dateWithLocale';
 
 /**
+ * Formats the date as a relative time if within 24 hours, otherwise formats as an absolute time.
+ */
+function formatTimeInfoDate(date) {
+  const now = new Date();
+  const rtf = new Intl.RelativeTimeFormat(process.env.LOCALE, { style: 'narrow' });
+  const dtf = new Intl.DateTimeFormat(process.env.LOCALE, { dateStyle: 'full', timeStyle: 'long' });
+  const minsAgo = (now - date) / 1000 / 60;
+  const hoursAgo = minsAgo / 60;
+
+  if (minsAgo < 1) {
+    return "less than a minute ago"
+  }
+  // "60 min. ago" and "1 hr. ago" mean the same thing, so if 59.5 <= minsAgo < 90 we display "1 hr. ago".
+  if (minsAgo < 59.5) {
+    return rtf.format(-Math.round(minsAgo), "minutes");
+  }
+  if (hoursAgo < 24) {
+    return rtf.format(-Math.round(hoursAgo), "hours");
+  }
+  return dtf.format(date);
+}
+
+/**
  * Add tooltip and renders date in preferred format
  *
  * @param {Date | string | number} props.time
@@ -18,11 +41,11 @@ function TimeInfo({ time, children = t => t }) {
     return <>{children(String(time))}</>;
   }
 
-  const timeAgoStr = formatDistanceToNow(date);
+  const timeAgoStr = formatTimeInfoDate(date);
 
   return (
     <Tooltip title={format(date)}>
-      <span>{children(format(date))}</span>
+      <span>{children(timeAgoStr)}</span>
     </Tooltip>
   );
 }
