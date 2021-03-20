0e90c32969f82fd08afa0c54bd33d49a9dd5a247
Kerrick Staley <kerrick@kerrickstaley.com>
2021-03-20 16:27:29 +0800

    Fix format issues and add comment

--- components/Infos/TimeInfo.js
+++ components/Infos/TimeInfo.js
@@ -1,17 +1,21 @@
 import Tooltip from 'components/Tooltip';
 import isValid from 'date-fns/isValid';
-import { format, formatDistanceToNow } from 'lib/dateWithLocale';
 import { useEffect, useState } from 'react';
 
 /**
  * Formats date as an absolute time, using local timezone.
  * Year will be omitted unless different from current year or forceYear = true.
  */
-function formatDateAbsolute(date, {forceYear = false} = {}) {
+function formatDateAbsolute(date, { forceYear = false } = {}) {
   const locale = process.env.LOCALE.replace('_', '-');
   const now = new Date();
 
-  let options = { day: 'numeric', month: 'short', hour: 'numeric', minute: 'numeric'};
+  let options = {
+    day: 'numeric',
+    month: 'short',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
   if (now.getFullYear() != date.getFullYear() || forceYear) {
     options.year = 'numeric';
   }
@@ -32,17 +36,17 @@ function formatDateRelative(date) {
   const hoursAgo = minsAgo / 60;
 
   if (minsAgo < 1) {
-    return "less than a minute ago"
+    return 'less than a minute ago';
   }
   // "60 min. ago" and "1 hr. ago" mean the same thing, so if 59.5 <= minsAgo < 90 we display "1 hr. ago".
   if (minsAgo < 59.5) {
-    return rtf.format(-Math.round(minsAgo), "minutes");
+    return rtf.format(-Math.round(minsAgo), 'minutes');
   }
   // Similar to above, use a cutoff of 23.5 hours.
   if (hoursAgo < 23.5) {
-    return rtf.format(-Math.round(hoursAgo), "hours");
+    return rtf.format(-Math.round(hoursAgo), 'hours');
   }
-  return rtf.format(-Math.round(hoursAgo / 24), "days");
+  return rtf.format(-Math.round(hoursAgo / 24), 'days');
 }
 
 /**
@@ -68,6 +72,13 @@ function TimeInfo({ time, children = t => t }) {
 
   const [timeAgoStr, setTimeAgoStr] = useState(formatDateRelative(date));
 
+  // The client and the server may have different timezones, so when we render on the server, we always use relative
+  // time (because it does not depend on timezone). When we re-render on the client, we replace this with an absolute
+  // time where appropriate. We do this replacement in useEffect() because it runs on the client but not on the server.
+  useEffect(() => {
+    setTimeAgoStr(formatDate(date));
+  }, [date]);
+
   if (!time || !isValid(date)) {
     // `time` may be falsy something not accepted by Date constructor.
     // Try rendering it anyway.
@@ -75,12 +86,8 @@ function TimeInfo({ time, children = t => t }) {
     return <>{children(String(time))}</>;
   }
 
-  useEffect(() => {
-    setTimeAgoStr(formatDate(date));
-  });
-
   return (
-    <Tooltip title={formatDateAbsolute(date, {forceYear: true})}>
+    <Tooltip title={formatDateAbsolute(date, { forceYear: true })}>
       <time dateTime={date.toISOString()}>{children(timeAgoStr)}</time>
     </Tooltip>
   );
