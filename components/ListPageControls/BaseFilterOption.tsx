d55daaf0cc865cdb7b61d366dbed619502ae3bc5
Johnson Liang <johnsonliang7@gmail.com>
2023-03-16 14:08:25 +0800

    refactor(ListPageControls) move BaseFilter and BaseFilterOption to Typescript

--- components/ListPageControls/BaseFilterOption.tsx
+++ components/ListPageControls/BaseFilterOption.tsx
@@ -1,17 +1,29 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
 
+type Props = {
+  /** toggle chip-like border on display */
+  chip?: boolean;
+  label: string;
+  value: string;
+  disabled?: boolean;
+  selected?: boolean;
+  onClick?: (value: string) => void;
+};
+
+type StyleProps = Pick<Props, 'selected' | 'chip'>;
+
 const useStyles = makeStyles(theme => ({
   root: {
     height: 'auto' /* Override material-ui */,
     padding: '3px 9px' /* consider 1px border */,
 
     // Add background color on selected
-    backgroundColor: ({ selected }) =>
+    backgroundColor: ({ selected }: StyleProps) =>
       selected ? theme.palette.secondary[50] : '',
 
     // Hide border color when not chip and not selected
-    borderColor: ({ chip, selected }) =>
+    borderColor: ({ chip, selected }: StyleProps) =>
       !chip && !selected ? `transparent` : '',
   },
   /* Chip label */
@@ -26,22 +38,14 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-/**
- * @param {boolean} props.chip - toggle chip-like border on display
- * @param {string} props.label
- * @param {string} props.value
- * @param {boolean} props.disabled
- * @param {boolean} props.selected
- * @param {(value: string) => void} props.onClick
- */
 function BaseFilterOption({
   chip,
   selected,
   label,
   value,
   disabled,
-  onClick = () => {},
-}) {
+  onClick = () => undefined,
+}: Props) {
   const classes = useStyles({ chip, selected });
   const handleClick = () => {
     onClick(value);
