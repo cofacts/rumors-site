cofacts/rumors-site

commit cf550a0ca17a8b6b44f7839454b1758704e701c0

[article] show blocked content only for blocked users

pages/article/[id].js
@@ -15,6 +15,7 @@ import useCurrentUser from 'lib/useCurrentUser';
 import { nl2br, linkify, ellipsis } from 'lib/text';
 import { usePushToDataLayer } from 'lib/gtm';
 import getTermsString from 'lib/terms';
+import { useIsUserBlocked } from 'lib/isUserBlocked';
 
 import { LINE_URL } from 'constants/urls';
 
@@ -106,7 +107,12 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const LOAD_ARTICLE = gql`
-  query LoadArticlePage($id: String!) {
+  query LoadArticlePage(
+    $id: String!
+    $replyRequestStatuses: [ReplyRequestStatusEnum!]
+    $articleReplyStatuses: [ArticleReplyStatusEnum!]
+    $articleCategoryStatuses: [ArticleCategoryStatusEnum!]
+  ) {
     GetArticle(id: $id) {
       id
       text
@@ -120,11 +126,11 @@ const LOAD_ARTICLE = gql`
       hyperlinks {
         ...HyperlinkData
       }
-      replyRequests {
+      replyRequests(statuses: $replyRequestStatuses) {
         reason
         ...ReplyRequestInfo
       }
-      articleReplies {
+      articleReplies(statuses: $articleReplyStatuses) {
         ...CurrentRepliesData
       }
       ...RelatedArticleData
@@ -140,7 +146,7 @@ const LOAD_ARTICLE = gql`
           }
         }
       }
-      articleCategories {
+      articleCategories(statuses: $articleCategoryStatuses) {
         ...ArticleCategoryData
         ...AddCategoryDialogData
       }
@@ -161,16 +167,21 @@ const LOAD_ARTICLE = gql`
 `;
 
 const LOAD_ARTICLE_FOR_USER = gql`
-  query LoadArticlePageForUser($id: String!) {
+  query LoadArticlePageForUser(
+    $id: String!
+    $replyRequestStatuses: [ReplyRequestStatusEnum!]
+    $articleReplyStatuses: [ArticleReplyStatusEnum!]
+    $articleCategoryStatuses: [ArticleCategoryStatusEnum!]
+  ) {
     GetArticle(id: $id) {
       id # Required, https://github.com/apollographql/apollo-client/issues/2510
-      replyRequests {
+      replyRequests(statuses: $replyRequestStatuses) {
         ...ReplyRequestInfoForUser
       }
-      articleReplies {
+      articleReplies(statuses: $articleReplyStatuses) {
         ...ArticleReplyForUser
       }
-      articleCategories {
+      articleCategories(statuses: $articleCategoryStatuses) {
         ...ArticleCategoryDataForUser
         ...AddCategoryDialogData
       }
@@ -182,11 +193,22 @@ const LOAD_ARTICLE_FOR_USER = gql`
   ${ArticleCategories.fragments.AddCategoryDialogData}
 `;
 
+const NORMAL_ONLY = ['NORMAL'];
+const NORMAL_AND_BLOCKED = ['NORMAL', 'BLOCKED'];
+
 function ArticlePage() {
   const { query } = useRouter();
   const [showForm, setShowForm] = useState(false);
   const [flashMessage, setFlashMessage] = useState(0);
-  const articleVars = { id: query.id };
+  const isUserBlocked = useIsUserBlocked();
+  const articleVars = {
+    id: query.id,
+    replyRequestStatuses: isUserBlocked ? NORMAL_AND_BLOCKED : NORMAL_ONLY,
+    articleCategoryStatuses: isUserBlocked ? NORMAL_AND_BLOCKED : NORMAL_ONLY,
+    articleReplyStatuses: isUserBlocked
+      ? ['NORMAL', 'BLOCKED', 'DELETED']
+      : ['NORMAL', 'DELETED'],
+  };
 
   const { data, loading } = useQuery(LOAD_ARTICLE, {
     variables: articleVars,

commit adb920fb8d24f7d433993b3f84a6e59270792c8d

Fix UI when adding categories to articles
Closes #481.

The reason the UI was not refreshed was a regression introduced by cf550a0ca17a8b6b44f7839454b1758704e701c0.
Adding the argument "(statuses: $articleCategoryStatuses)" changes the key used by InMemoryCache
from "articleCategories" to "articleCategories({"statuses":["NORMAL"]})".
This results in cache.readFragment() (called in components/ArticleCategories/CategoryOption.js throwing this error:
```
Invariant Violation: Can't find field articleCategories on object {
  "id": ...,
  "text": ...
  ...
  "articleCategories({\"statuses\":[\"NORMAL\"]})": [
    {
      "type": "id",
      "generated": false,
      "id": "ArticleCategory:...__...",
      "typename": "ArticleCategory"
    }
  ],
  "stats": [
    ...
  ],
  "user": {
    ...
  }
}
```

This commits solves this issue by forcing the key to be "articleCategories".

The technique of using an @connection directive is documented here:
https://www.apollographql.com/docs/react/data/directives/#connection

pages/article/[id].js
@@ -173,7 +173,7 @@ const LOAD_ARTICLE = gql`
       cooccurrences {
         ...CooccurrenceSectionData
       }
-      articleCategories(statuses: $articleCategoryStatuses) {
+      articleCategories(statuses: $articleCategoryStatuses) @connection(key: "articleCategories") {
         ...ArticleCategoryData
         ...AddCategoryDialogData
       }
@@ -221,7 +221,7 @@ const LOAD_ARTICLE_FOR_USER = gql`
       articleReplies(statuses: $articleReplyStatuses) {
         ...ArticleReplyForUser
       }
-      articleCategories(statuses: $articleCategoryStatuses) {
+      articleCategories(statuses: $articleCategoryStatuses) @connection(key: "articleCategories") {
         ...ArticleCategoryDataForUser
         ...AddCategoryDialogData
       }